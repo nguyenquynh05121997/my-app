7099fc3694dc54855306dda40a11a82c1fbb6ccd
nguyen van quynh <124028993+nguyenquynh05121997@users.noreply.github.com>
2023-07-12 23:17:02 +0900

    Create Global Style and reset Css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import logo from "./logo.svg";
-import "~/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <h1>hello ae</h1>
   );
 }
 
